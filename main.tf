5d6adbe4c180a3105962f97b08cb3516f41f5b93
thelooneytoons <63376600+thelooneytoons@users.noreply.github.com>
2022-04-21 18:51:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+
+
 provider "google" {
     version = "3.5.0"
     #credentials = file("/downloads/compute-instance.json")
@@ -6,12 +8,14 @@ provider "google" {
     zone = "us-central1-c"
 }
 
-resource "google_compute_global_address" "default" {
-  project      =  "training-freshers" # Replace this with your service project ID in quotes
-  name         = "ipv6-address"
-  address_type = "EXTERNAL"
-  ip_version   = "IPV4"
-}
+
+
+
+ resource "google_compute_address" "static-dvs" {
+   project      =  "training-freshers" # Replace this with your service project ID in quotes
+   name         = "ipv4-address"
+   address_type = "EXTERNAL"
+ }
 
 resource "google_compute_instance" "default" {
   name         = "thelooneytoons2"
@@ -30,18 +34,22 @@ resource "google_compute_instance" "default" {
     network = "default"
 
     access_config {
-      nat_ip = google_compute_global_address.default.address
+       nat_ip = google_compute_address.static-dvs.address
     }
   }
 
   // Apply the firewall rule to allow external IPs to access this instance
-  tags = ["default-allow-http"]
+  tags = ["http-server"]
 }
 
 
 output "ip" {
-  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+   value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
+
+
+
+
 
 
 
