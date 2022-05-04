efdfbf0bc53aa9a98032bd9c257b61cf32eadd71
thelooneytoons <63376600+thelooneytoons@users.noreply.github.com>
2022-05-04 14:47:59 +0530

    final commit

--- main.tf
+++ main.tf
@@ -1,23 +1,15 @@
-
-
 provider "google" {
     version = "3.5.0"
-  
     project = "thelooneytoons-tasks"
     region = "us-central1"
     zone = "us-central1-c"
 }
 
 
-
-
- resource "google_compute_address" "static-dvs" {
-   project      =  "thelooneytoons-tasks" # Replace this with your service project ID in quotes
-   name         = "ipv4-address"
-   address_type = "EXTERNAL"
+resource "google_compute_address" "static-dvs" {
+  name         = "ipv4-address"
+  address_type = "EXTERNAL"
  }
-
-
 
 
 resource "google_compute_instance" "default" {
@@ -40,11 +32,6 @@ resource "google_compute_instance" "default" {
        nat_ip = google_compute_address.static-dvs.address
     }
   }
-    tags= ["http-server"]
+
+  tags = ["allow-http"]
 }
-
-
-
-
-
-
