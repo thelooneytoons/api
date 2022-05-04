8882c76ff41a43117f025614e0052efdbc2d3a11
thelooneytoons <63376600+thelooneytoons@users.noreply.github.com>
2022-05-04 11:40:59 +0530

    iam policy regarding storage is updated

--- main.tf
+++ main.tf
@@ -63,6 +63,21 @@ resource "google_compute_instance" "default" {
 # }
 
 
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/storage.admin"
+    role = "roles/storage.objectCreator"
+    role = "roles/storage.objectAdmin"
+    #[projects|organizations]/{parent-name}/roles/{role-name}
+    members = [
+      "user:sotrage-bucket@thelooneytoons-tasks.iam.gserviceaccount.com",
+    ]
+  }
+}
 
+resource "google_storage_bucket_iam_policy" "policy" {
+  bucket = google_storage_bucket.default.name
+  policy_data = data.google_iam_policy.admin.policy_data
+}
 
 
